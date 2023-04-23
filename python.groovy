c2e4469053343da6b6087532aebaefb8fa76066e
NKChowdary9 <131529914+NKChowdary9@users.noreply.github.com>
2023-04-23 15:51:26 +0530

    Update python.groovy

--- python.groovy
+++ python.groovy
@@ -1,4 +1,4 @@
-def add(basic,travel){
-  echo "ramana salary ${basic} and ${travel} is ${basic + travel}
+def add(basic,travel) {
+  echo "ramana salary ${basic} and ${travel} is ${basic+travel}
 }
 
